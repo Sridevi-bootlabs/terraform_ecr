21dc26af52f7a8ae5f5f71e61757d4abeaac6a4b
Sridevi-bootlabs <105430846+Sridevi-bootlabs@users.noreply.github.com>
2022-09-30 11:30:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,12 @@
 resource "aws_ecr_repository" "ecr" {
   name = var.name
   image_tag_mutability = var.image_tag_mutability
+ 
+  encrytion_configuration{
+    encryption_type = var.encryption_type
+    kms_key         = var.kms_key
+  }
+  
   image_scanning_configuration {
     scan_on_push = var.scan_on_push
   }
