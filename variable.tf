e0fdf8a1dc764251d6b250ea6d10592e19bf53ef
Sridevi-bootlabs <105430846+Sridevi-bootlabs@users.noreply.github.com>
2022-09-30 11:36:02 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -10,3 +10,14 @@ variable "scan_on_push" {
   description = "Indicates whether images are scanned after being pushed to the repository (true) or not scanned (false)"
   type        = bool
 }
+variable "encryption_type" {
+  description = "The encryption type to use for the repository. Valid values are AES256 or KMS. Defaults to AES256"
+  type        = string
+  default     = "AES256"
+}
+
+variable "kms_key" {
+  description = "The ARN of the KMS key to use when encryption_type is KMS. If not specified, uses the default AWS managed key for ECR"
+  type        = string
+  default     = ""
+}
